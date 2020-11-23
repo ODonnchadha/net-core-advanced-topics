15a0f9df183861a7c00dc6ccb8fd7c8b3f2d322d
ODonnchadha <danieldonahue1231@hotmail.com>
2020-11-23 06:42:30 -0600

    INIT.

---
+++ Advanced.Generics/Nullable.cs
@@ -0,0 +1,28 @@
+﻿namespace Advanced.Generics
+{
+    /// <summary>
+    /// We 'box' to store the value and 'unbox' within the GetValueOrDefault() method.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Nullable<T> where T : struct
+    {
+        private object _value;
+        public Nullable() { }
+        public Nullable(T value) => _value = value;
+        public bool HasValue() => _value != null;
+
+        /// <summary>
+        /// Note the 'default' keyword.
+        /// </summary>
+        /// <returns></returns>
+        public T GetValueOrDefault()
+        {
+            if (HasValue())
+            {
+                return (T)_value;
+            }
+                
+            return default(T);
+        }
+    }
+}
---
+++ Advanced.Generics/Utilities.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Advanced.Generics
+{
+    public class Utilities
+    {
+        public T Max<T>(T a, T b) where T : IComparable => a.CompareTo(b) > 0 ? a : b;
+    }
+}

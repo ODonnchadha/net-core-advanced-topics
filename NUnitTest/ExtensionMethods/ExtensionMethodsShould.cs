fbaa3ab113cdada4be91617629257692acce8236
ddonahue <ddonahue@questarai.com>
2020-11-23 13:55:28 -0600

    INIT.

---
+++ NUnitTest/ExtensionMethods/ExtensionMethodsShould.cs
@@ -0,0 +1,22 @@
+﻿using Advanced.ExtensionMethods;
+using NUnit.Framework;
+
+namespace NUnitTest.Delegates
+{
+    public class ExtensionMethodsShould
+    {
+        private string words;
+
+        [SetUp()]
+        public void Setup()
+        {
+            this.words = "In my younger and more vulnerable years my father gave me some advice that I've been turning over in my mind ever since.";
+        }
+
+        [Test()]
+        public void Shorten()
+        {
+            Assert.AreEqual(words.Shorten(7), "In my younger and more vulnerable years...");
+        }
+    }
+}
